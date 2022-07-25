df082fdd109506f70506aeb1191551ee8473f7f4
ttt-net <tenten102matsuyama@gmail.com>
2022-07-25 19:42:16 +0900

    Add dice image to View

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         ZStack {
             Image("background")
                 .resizable()
                 .ignoresSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                
+                HStack {
+                    Image("dice1")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .padding()
+                    Image("dice6")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .padding()
+                }
+                .padding(.horizontal)
+            }
         }
     }
 }
